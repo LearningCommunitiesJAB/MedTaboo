00a76844ce198cdd59de08b5ed21aa1014473098
LearningCommunitiesJAB <hao4@hawaii.edu>
2026-06-30 17:57:44 -1000

    Create mdt-cards.js

---
+++ js/data/mdt-cards.js
@@ -0,0 +1,202 @@
+[
+  {
+    "guessWord": "Appendicitis",
+    "tabooWords": ["appendix", "belly", "pain", "burst", "surgery"]
+  },
+  {
+    "guessWord": "Stethoscope",
+    "tabooWords": ["listen", "heart", "lungs", "doctor", "ears"]
+  },
+  {
+    "guessWord": "Diabetes",
+    "tabooWords": ["sugar", "insulin", "blood", "needle", "pancreas"]
+  },
+  {
+    "guessWord": "Jaundice",
+    "tabooWords": ["yellow", "skin", "eyes", "liver", "baby"]
+  },
+  {
+    "guessWord": "Scoliosis",
+    "tabooWords": ["spine", "back", "curve", "crooked", "brace"]
+  },
+  {
+    "guessWord": "Pneumonia",
+    "tabooWords": ["lungs", "cough", "fluid", "sick", "breathing"]
+  },
+  {
+    "guessWord": "Anesthesia",
+    "tabooWords": ["sleep", "pain", "surgery", "numb", "knockout"]
+  },
+  {
+    "guessWord": "Concussion",
+    "tabooWords": ["brain", "head", "hit", "injury", "helmet"]
+  },
+  {
+    "guessWord": "EpiPen",
+    "tabooWords": ["allergic", "peanut", "shot", "bee", "emergency"]
+  },
+  {
+    "guessWord": "Scurvy",
+    "tabooWords": ["pirates", "vitamin C", "orange", "fruit", "teeth"]
+  },
+  {
+    "guessWord": "Placebo",
+    "tabooWords": ["fake", "pill", "sugar", "mind", "trick"]
+  },
+  {
+    "guessWord": "Alzheimer's",
+    "tabooWords": ["memory", "forget", "old", "grandpa", "brain"]
+  },
+  {
+    "guessWord": "Asthma",
+    "tabooWords": ["inhaler", "breathe", "puff", "lungs", "wheeze"]
+  },
+  {
+    "guessWord": "CPR",
+    "tabooWords": ["chest", "pump", "breathing", "heart", "save"]
+  },
+  {
+    "guessWord": "Thermometer",
+    "tabooWords": ["fever", "temperature", "hot", "degree", "mouth"]
+  },
+  {
+    "guessWord": "Antibiotic",
+    "tabooWords": ["bacteria", "pill", "infection", "sick", "prescription"]
+  },
+  {
+    "guessWord": "Vaccine",
+    "tabooWords": ["shot", "needle", "immunity", "virus", "sick"]
+  },
+  {
+    "guessWord": "Migraine",
+    "tabooWords": ["headache", "pain", "light", "brain", "severe"]
+  },
+  {
+    "guessWord": "Allergy",
+    "tabooWords": ["sneeze", "pollen", "peanut", "hives", "itchy"]
+  },
+  {
+    "guessWord": "Braces",
+    "tabooWords": ["teeth", "wire", "smile", "straighten", "metal"]
+  },
+  {
+    "guessWord": "Cavity",
+    "tabooWords": ["tooth", "decay", "sugar", "dentist", "hole"]
+  },
+  {
+    "guessWord": "Acne",
+    "tabooWords": ["pimple", "face", "skin", "teenager", "pop"]
+  },
+  {
+    "guessWord": "Dehydration",
+    "tabooWords": ["water", "thirsty", "drink", "sweat", "dry"]
+  },
+  {
+    "guessWord": "Insomnia",
+    "tabooWords": ["sleep", "night", "awake", "tired", "bed"]
+  },
+  {
+    "guessWord": "Lactose Intolerant",
+    "tabooWords": ["milk", "dairy", "cheese", "stomach", "ice cream"]
+  },
+  {
+    "guessWord": "Cast",
+    "tabooWords": ["broken", "bone", "arm", "leg", "plaster"]
+  },
+  {
+    "guessWord": "X-Ray",
+    "tabooWords": ["bone", "picture", "broken", "radiation", "machine"]
+  },
+  {
+    "guessWord": "Ligament",
+    "tabooWords": ["bone", "joint", "connect", "sprain", "ACL"]
+  },
+  {
+    "guessWord": "Tendon",
+    "tabooWords": ["muscle", "bone", "connect", "Achilles", "strain"]
+  },
+  {
+    "guessWord": "Blood Pressure",
+    "tabooWords": ["cuff", "arm", "heart", "high", "number"]
+  },
+  {
+    "guessWord": "Pulse",
+    "tabooWords": ["heartbeat", "wrist", "neck", "count", "rate"]
+  },
+  {
+    "guessWord": "Lyme Disease",
+    "tabooWords": ["tick", "bite", "woods", "bullseye", "rash"]
+  },
+  {
+    "guessWord": "Hypothermia",
+    "tabooWords": ["cold", "freezing", "snow", "temperature", "shiver"]
+  },
+  {
+    "guessWord": "Stitches",
+    "tabooWords": ["cut", "sew", "wound", "scar", "thread"]
+  },
+  {
+    "guessWord": "Melatonin",
+    "tabooWords": ["sleep", "gummy", "night", "hormone", "bedtime"]
+  },
+  {
+    "guessWord": "Amnesia",
+    "tabooWords": ["forget", "memory", "remember", "head", "blank"]
+  },
+  {
+    "guessWord": "Carpal Tunnel",
+    "tabooWords": ["wrist", "typing", "keyboard", "pain", "hand"]
+  },
+  {
+    "guessWord": "Pink Eye",
+    "tabooWords": ["itchy", "crusty", "contagious", "red", "tear"]
+  },
+  {
+    "guessWord": "Conjoined Twins",
+    "tabooWords": ["stuck", "born", "together", "attached", "two"]
+  },
+  {
+    "guessWord": "Mono",
+    "tabooWords": ["kissing", "disease", "tired", "saliva", "teenager"]
+  },
+  {
+    "guessWord": "First Aid Kit",
+    "tabooWords": ["band-aid", "gauze", "emergency", "box", "hurt"]
+  },
+  {
+    "guessWord": "Stomach Ulcer",
+    "tabooWords": ["acid", "hole", "pain", "burning", "belly"]
+  },
+  {
+    "guessWord": "Crutches",
+    "tabooWords": ["walk", "broken", "leg", "armpits", "injury"]
+  },
+  {
+    "guessWord": "Anemia",
+    "tabooWords": ["iron", "blood", "tired", "pale", "red cells"]
+  },
+  {
+    "guessWord": "Heart Attack",
+    "tabooWords": ["chest", "pain", "cardiac", "clot", "stop"]
+  },
+  {
+    "guessWord": "Stroke",
+    "tabooWords": ["brain", "clot", "face", "slur", "paralysis"]
+  },
+  {
+    "guessWord": "Band-Aid",
+    "tabooWords": ["cut", "scrape", "bleed", "sticky", "bandage"]
+  },
+  {
+    "guessWord": "Bruise",
+    "tabooWords": ["purple", "skin", "bump", "color", "hit"]
+  },
+  {
+    "guessWord": "Burn",
+    "tabooWords": ["fire", "hot", "stove", "skin", "blister"]
+  },
+  {
+    "guessWord": "Seizure",
+    "tabooWords": ["shake", "epilepsy", "brain", "fit", "convulsion"]
+  }
+]
